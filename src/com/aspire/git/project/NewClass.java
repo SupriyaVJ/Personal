42c2d3daeaba55dc28a31b8d78c384772c9a6a0b
User <User@User-PC>
2022-11-15 20:20:28 +0530

    edited

--- src/com/aspire/git/project/NewClass.java
+++ src/com/aspire/git/project/NewClass.java
@@ -3,5 +3,6 @@ package com.aspire.git.project;
 public class NewClass {
 	public static void main(String[] args) {
 		System.out.println("hi this is my firt push code");
+		System.out.println("hi this is my 2 nd push");
 	}
 }
